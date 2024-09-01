cb8cd75b5c77fe9a19fbebec302aedce05fdffab
Sonu Sharma <136222449+Sonu0Sharma@users.noreply.github.com>
2024-09-02 04:44:03 +0530

    Create Question_ 3-solution.sql

---
+++ Question_ 3-solution.sql
@@ -0,0 +1,13 @@
+-- Q.3. Identify the highest-priced pizza.
+
+
+SELECT 
+    (pizza_types.name) AS highest_price_pizza_name, pizzas.price
+FROM
+    pizza_types
+        JOIN
+    pizzas 
+
+ ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+ORDER BY pizzas.price DESC
+LIMIT 1;
