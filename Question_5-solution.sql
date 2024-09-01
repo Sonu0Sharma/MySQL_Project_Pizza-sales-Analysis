d9df1cc434f640b26f665f0de43a4a734b99c81f
Sonu Sharma <136222449+Sonu0Sharma@users.noreply.github.com>
2024-09-02 04:51:09 +0530

    Create Question_5-solution.sql

---
+++ Question_5-solution.sql
@@ -0,0 +1,21 @@
+-- Q.5. List the top 5 most ordered pizza types along with their quantities.
+
+ select pizza_types.name,
+ sum(order_details.quantity) as quantity
+ 
+ from pizza_types join pizzas
+ on pizza_types.pizza_type_id = pizzas.pizza_type_id
+ 
+ join order_details
+ on order_details.pizza_id = pizzas.pizza_id
+ 
+ group by pizza_types.name order by quantity desc limit 5;
+
+
+-- Result:
+
+'The Classic Deluxe Pizza','2453'
+'The Barbecue Chicken Pizza','2432'
+'The Hawaiian Pizza','2422'
+'The Pepperoni Pizza','2418'
+'The Thai Chicken Pizza','2371'
